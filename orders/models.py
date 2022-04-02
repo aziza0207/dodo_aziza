1012ad7b388fb6d46d2013c82f73e004f5d30226
Aziza <azizamukashkyzy@gmail.com>
2022-04-03 00:37:39 +0600

    models for order app created

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from enum import Enum
+from dishes.models import Dish
+
+
+class Cart(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    DOUGH_TYPE_CHOICES = [
+        ("traditional", "Традиционное"),
+        ("slim", "Тонкое")]
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    dish = models.ForeignKey("dishes.Dish", on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    dough_type = models.CharField(max_length=100, choices=DOUGH_TYPE_CHOICES, null=True)
+    dish_size = models.ForeignKey("dishes.DishSize", on_delete=models.SET_NULL, null=True)
+
